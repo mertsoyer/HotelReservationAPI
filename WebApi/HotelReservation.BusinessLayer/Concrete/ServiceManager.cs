240acaff4dab241fdc83eb7e4ee5d58445b81f06
Mert Soyer <mertsoyer@hotmail.com.tr>
2023-04-12 15:22:37 +0300

    Serviceler oluşturuldu

---
+++ WebApi/HotelReservation.BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelReservation.BusinessLayer.Abstract;
+using HotelReservation.DataAccessLayer.Abstract;
+using HotelReservation.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.BusinessLayer.Concrete
+{
+    public class ServiceManager : IServiceService
+    {
+        private readonly IServicesDal _servicesDal;
+
+        public ServiceManager(IServicesDal servicesDal)
+        {
+            _servicesDal = servicesDal;
+        }
+
+        public void TDelete(Service entity)
+        {
+           _servicesDal.Delete(entity);
+        }
+
+        public Service TGetById(int id)
+        {
+            return _servicesDal.GetById(id);
+        }
+
+        public List<Service> TGetList()
+        {
+            return _servicesDal.GetList();
+        }
+
+        public void TInsert(Service entity)
+        {
+           _servicesDal.Insert(entity);
+        }
+
+        public void TUpdate(Service entity)
+        {
+           _servicesDal.Update(entity);
+        }
+    }
+}
