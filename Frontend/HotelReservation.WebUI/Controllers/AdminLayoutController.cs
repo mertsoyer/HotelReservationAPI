e869a565650a3439d427241cb756ad58cc096e83
Mert Soyer <mertsoyer@hotmail.com.tr>
2023-04-13 12:04:47 +0300

    admin teması

---
+++ Frontend/HotelReservation.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+    }
+}
